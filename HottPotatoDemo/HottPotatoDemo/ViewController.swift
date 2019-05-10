1ab557327451bbf52eb914edbf0b6aaa5d63d71c
Harlan Kellaway <harlan@prolificinteractive.com>
2019-05-10 14:50:01 -0400

    Created demo project

---
+++ HottPotatoDemo/HottPotatoDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HottPotatoDemo
+//
+//  Created by Harlan Kellaway on 5/10/19.
+//  Copyright © 2019 Harlan Kellaway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
