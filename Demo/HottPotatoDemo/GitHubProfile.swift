b84ca9ebf9b5703afbc78ca607a033b35734ec82
Harlan Kellaway <harlan@prolificinteractive.com>
2019-05-10 17:26:21 -0400

    Added HTTP request to demo

---
+++ Demo/HottPotatoDemo/GitHubProfile.swift
@@ -0,0 +1,16 @@
+//
+//  GitHubProfile.swift
+//  HottPotatoDemo
+//
+//  Created by Harlan Kellaway on 5/10/19.
+//  Copyright © 2019 Harlan Kellaway. All rights reserved.
+//
+
+import Foundation
+
+struct GitHubProfile: Codable {
+    
+    let id: Int
+    let login: String
+    
+}
