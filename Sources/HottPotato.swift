fec3bc6c4d95ded41e1a119e4888c7f95880b995
Harlan Kellaway <hello@harlankellaway.com>
2019-05-11 00:44:05 -0400

    Docs

--- Sources/HottPotato.swift
+++ Sources/HottPotato.swift
@@ -2,8 +2,25 @@
 //  HottPotato.swift
 //  HottPotato
 //
-//  Created by Harlan Kellaway on 5/10/19.
-//  Copyright © 2019 Harlan Kellaway. All rights reserved.
+// Copyright (c) 2019 Harlan Kellaway
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 //
 
 import Foundation
@@ -71,6 +88,8 @@ public final class HottPotato: JSONHTTPClient {
     public func sendJSONRequest(with request: HTTPRequest, success: @escaping (_ data: JSONData, _ json: JSON) -> (), failure: @escaping (Error) -> ()) {
         requestSender.sendJSONRequest(with: request, success: success, failure: failure)
     }
+    
+    // MARK: HTTPClient
     
     public func sendHTTPRequest(with request: HTTPRequest, success: @escaping (Data, HTTPURLResponse) -> (), failure: @escaping (HTTPClientError) -> ()) {
         requestSender.sendHTTPRequest(with: request, success: success, failure: failure)
